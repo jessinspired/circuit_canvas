898e1ac7f356fdddbb354bb061289378f95b81fa
Obelem <ibbraidejesse@gmail.com>
2025-03-14 18:22:20 +0100

    added scroll course btns

--- scripts/home/coursesScroll.js
+++ scripts/home/coursesScroll.js
@@ -3,7 +3,16 @@ document.addEventListener("DOMContentLoaded", function () {
   const nextBtn = document.getElementById("next-btn");
   const prevBtn = document.getElementById("prev-btn");
 
-  const scrollAmount = container.offsetWidth * 0.3; // Scroll by 50% of container width
+  const scrollAmount = container.offsetWidth * 0.5; // Scroll by 50% of container width
+
+  function checkScroll() {
+    prevBtn.disabled = container.scrollLeft <= 0;
+    nextBtn.disabled =
+      container.scrollLeft + container.clientWidth >= container.scrollWidth;
+
+    prevBtn.style.backgroundColor = prevBtn.disabled ? "#ccc" : "#2E1562";
+    nextBtn.style.backgroundColor = nextBtn.disabled ? "#ccc" : "#2E1562";
+  }
 
   nextBtn.addEventListener("click", function () {
     container.scrollBy({ left: scrollAmount, behavior: "smooth" });
@@ -12,4 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
   prevBtn.addEventListener("click", function () {
     container.scrollBy({ left: -scrollAmount, behavior: "smooth" });
   });
+
+  container.addEventListener("scroll", checkScroll);
+  checkScroll(); // Initial check
 });
